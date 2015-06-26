7604e9997bc381bfbc770169d4e09a37c9677a59
Freeman77 <pabloascencio77@hotmail.com>
2015-06-25 21:19:14 -0400

    Fix include que se mostraba en home.php

--- php/libClases.php
+++ php/libClases.php
@@ -1,5 +1,5 @@
 <?php
-include './constantes.php';
+include_once './constantes.php';
 include_once PATHAPP. 'clases/Producto.php';
 include_once PATHAPP. 'clases/Categoria.php';
 include_once PATHAPP. 'lib/db_funciones.php';
